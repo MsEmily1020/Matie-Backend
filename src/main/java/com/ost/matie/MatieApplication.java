4a1c18f910ea801b7ed3e493fb6c27265621c63c
MsEmily1020 <peace10200khs@gmail.com>
2024-01-09 18:46:16 +0900

    🎉 프로젝트 초기 환경 셋팅

---
+++ src/main/java/com/ost/matie/MatieApplication.java
@@ -0,0 +1,13 @@
+package com.ost.matie;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MatieApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MatieApplication.class, args);
+	}
+
+}
