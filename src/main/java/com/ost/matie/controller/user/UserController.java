7607a499c231e0cc7cec041e692bdca1d9f52d46
MsEmily1020 <peace10200khs@gmail.com>
2024-01-10 22:24:49 +0900

    ✨ user controller - get 추가

--- src/main/java/com/ost/matie/controller/user/UserController.java
+++ src/main/java/com/ost/matie/controller/user/UserController.java
@@ -2,14 +2,18 @@ package com.ost.matie.controller.user;
 
 import com.ost.matie.domain.user.Users;
 import com.ost.matie.dto.user.AddUserRequest;
+import com.ost.matie.dto.user.UserResponse;
 import com.ost.matie.service.user.UserService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RequiredArgsConstructor
 @RestController
@@ -21,5 +25,15 @@ public class UserController {
         Users users = userService.save(request);
         return ResponseEntity.status(HttpStatus.CREATED)
                 .body(users);
+    }
+
+    @GetMapping("/users")
+    public ResponseEntity<List<UserResponse>> findAllUsers() {
+        List<UserResponse> userResponses = userService.findAll()
+                .stream()
+                .map(UserResponse::new)
+                .toList();
+
+        return ResponseEntity.ok().body(userResponses);
     }
 }
