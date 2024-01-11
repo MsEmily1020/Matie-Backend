3939a584a5d36a5c39d44177d39bd071adcd5809
MsEmily1020 <peace10200khs@gmail.com>
2024-01-11 13:50:12 +0900

    ✨ team service - update 추가

--- src/main/java/com/ost/matie/service/team/TeamService.java
+++ src/main/java/com/ost/matie/service/team/TeamService.java
@@ -1,7 +1,11 @@
 package com.ost.matie.service.team;
 
 import com.ost.matie.domain.team.Team;
+import com.ost.matie.domain.user.Users;
+import com.ost.matie.dto.team.UpdateTeamRequest;
+import com.ost.matie.dto.user.UpdateUserRequest;
 import com.ost.matie.repository.TeamRepository;
+import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -13,4 +17,14 @@ public class TeamService {
     private final TeamRepository teamRepository;
 
     public List<Team> findAll() { return teamRepository.findAll(); }
+
+    @Transactional
+    public Team update(Long id, UpdateTeamRequest request) {
+        Team team = teamRepository.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("not found " + id));
+
+        team.update(request.getUserList());
+
+        return team;
+    }
 }
