13a7aecc44011950cc3fbe9e49462dcc38b84340
MsEmily1020 <peace10200khs@gmail.com>
2024-01-11 13:15:06 +0900

    ✨ point repository - findByUserIdOrderByCreatedDateDesc 추가

--- src/main/java/com/ost/matie/repository/PointRepository.java
+++ src/main/java/com/ost/matie/repository/PointRepository.java
@@ -3,5 +3,8 @@ package com.ost.matie.repository;
 import com.ost.matie.domain.point.Point;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface PointRepository extends JpaRepository<Point, Long> {
+    List<Point> findByUserIdOrderByCreatedDateDesc();
 }
