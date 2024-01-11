b94c75465b9d6ccc9d56ee84d9ef72c4d129b1c6
MsEmily1020 <peace10200khs@gmail.com>
2024-01-11 13:18:58 +0900

    ✨ point controller - addPoint 추가

---
+++ src/main/java/com/ost/matie/controller/point/PointController.java
@@ -0,0 +1,28 @@
+package com.ost.matie.controller.point;
+
+import com.ost.matie.domain.point.Point;
+import com.ost.matie.domain.user.Users;
+import com.ost.matie.dto.point.AddPointRequest;
+import com.ost.matie.dto.user.AddUserRequest;
+import com.ost.matie.repository.PointRepository;
+import com.ost.matie.service.point.PointService;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequiredArgsConstructor
+@RestController
+public class PointController {
+    private final PointService pointService;
+
+    @PostMapping("/point")
+    public ResponseEntity<Point> addPoint(@Valid @RequestBody AddPointRequest request) {
+        Point point = pointService.save(request);
+        return ResponseEntity.status(HttpStatus.CREATED)
+                .body(point);
+    }
+}
