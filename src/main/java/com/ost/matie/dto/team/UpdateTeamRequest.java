f5bc2c39e3f1408ad736bb32687a087aa7f7b4c1
MsEmily1020 <peace10200khs@gmail.com>
2024-01-11 13:46:40 +0900

    ✨ team dto - update request 추가

--- src/main/java/com/ost/matie/dto/team/UpdateTeamRequest.java
+++ src/main/java/com/ost/matie/dto/team/UpdateTeamRequest.java
@@ -1,4 +1,14 @@
 package com.ost.matie.dto.team;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
 public class UpdateTeamRequest {
+    private List<Long> userList;
 }
