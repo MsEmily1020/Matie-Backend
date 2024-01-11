f33678434313c2024c84da9352040b44c14e0646
MsEmily1020 <peace10200khs@gmail.com>
2024-01-11 13:00:59 +0900

    ✨ point dto - add request 추가

---
+++ src/main/java/com/ost/matie/dto/point/AddPointRequest.java
@@ -0,0 +1,23 @@
+package com.ost.matie.dto.point;
+
+import com.ost.matie.domain.user.Users;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+public class AddPointRequest {
+    private Long variation;
+
+    @Min(0)
+    private Long balance;
+
+    @NotNull
+    private Users user;
+}
