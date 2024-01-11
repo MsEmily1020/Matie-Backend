d458521045387111ec360ea91f85b781ec08aca5
MsEmily1020 <peace10200khs@gmail.com>
2024-01-11 18:54:23 +0900

    ✨ community controller - findByTypeOrderbyDateDesc 추가

--- src/main/java/com/ost/matie/controller/community/CommunityController.java
+++ src/main/java/com/ost/matie/controller/community/CommunityController.java
@@ -3,15 +3,17 @@ package com.ost.matie.controller.community;
 import com.ost.matie.domain.community.Community;
 import com.ost.matie.domain.point.Point;
 import com.ost.matie.dto.community.AddCommunityRequest;
+import com.ost.matie.dto.community.CommunityResponse;
 import com.ost.matie.dto.point.AddPointRequest;
+import com.ost.matie.dto.point.PointResponse;
 import com.ost.matie.service.community.CommunityService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RequiredArgsConstructor
 @RestController
@@ -23,5 +25,15 @@ public class CommunityController {
         Community community = communityService.save(request);
         return ResponseEntity.status(HttpStatus.CREATED)
                 .body(community);
+    }
+
+    @GetMapping("/community/{type}")
+    public ResponseEntity<List<CommunityResponse>> findByTypeOrderByDateDesc(@PathVariable String type) {
+        List<CommunityResponse> communityResponses = communityService.findByTypeOrderByCreatedDateDesc(type)
+                .stream()
+                .map(CommunityResponse::new)
+                .toList();
+
+        return ResponseEntity.ok().body(communityResponses);
     }
 }
