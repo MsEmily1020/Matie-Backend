fddd7fda2d28f7e0b49bb9a60e0fd3c447724661
MsEmily1020 <peace10200khs@gmail.com>
2024-01-12 19:14:45 +0900

    ✨ comment dto - response 추가

---
+++ src/main/java/com/ost/matie/dto/comment/CommentResponse.java
@@ -0,0 +1,32 @@
+package com.ost.matie.dto.comment;
+
+import com.ost.matie.domain.comment.Comment;
+import com.ost.matie.domain.community.Community;
+import com.ost.matie.domain.user.Users;
+import lombok.Getter;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Getter
+public class CommentResponse {
+    private final Long id;
+    private final String description;
+    private final Long upvotes;
+    private final List<Long> upvoteUserList;
+    private final LocalDateTime createdDate;
+    private final LocalDateTime modifiedDate;
+    private final Users user;
+    private final Community community;
+
+    public CommentResponse(Comment comment) {
+        this.id = comment.getId();
+        this.description = comment.getDescription();
+        this.upvotes = comment.getUpvotes();
+        this.upvoteUserList = comment.getUpvoteUserList();
+        this.createdDate = comment.getCreatedDate();
+        this.modifiedDate = comment.getModifiedDate();
+        this.user = comment.getUser();
+        this.community = comment.getCommunity();
+    }
+}
