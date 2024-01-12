bac9dc65db90f60da8612cf298b342541561a9bb
MsEmily1020 <peace10200khs@gmail.com>
2024-01-12 19:20:24 +0900

    ✨ comment dto - add request toEntity 추가

--- src/main/java/com/ost/matie/dto/comment/AddCommentRequest.java
+++ src/main/java/com/ost/matie/dto/comment/AddCommentRequest.java
@@ -1,8 +1,8 @@
 package com.ost.matie.dto.comment;
 
+import com.ost.matie.domain.comment.Comment;
 import com.ost.matie.domain.community.Community;
 import com.ost.matie.domain.user.Users;
-import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
@@ -10,7 +10,7 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
-import java.util.List;
+import java.util.Collections;
 
 @NoArgsConstructor
 @AllArgsConstructor
@@ -25,4 +25,14 @@ public class AddCommentRequest {
 
     @NotNull
     private Community community;
+
+    public Comment toEntity() {
+        return Comment.builder()
+                .description(description)
+                .upvotes(0L)
+                .upvoteUserList(Collections.emptyList())
+                .user(user)
+                .community(community)
+                .build();
+    }
 }
