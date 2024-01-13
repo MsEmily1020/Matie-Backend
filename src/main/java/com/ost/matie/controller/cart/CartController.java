08c5f0c35f30538cf17f362c7df339b6a57cee84
MsEmily1020 <peace10200khs@gmail.com>
2024-01-13 22:21:43 +0900

    ✨ cart controller - findAllByUserI 추가

--- src/main/java/com/ost/matie/controller/cart/CartController.java
+++ src/main/java/com/ost/matie/controller/cart/CartController.java
@@ -3,15 +3,17 @@ package com.ost.matie.controller.cart;
 import com.ost.matie.domain.cart.Cart;
 import com.ost.matie.domain.comment.Comment;
 import com.ost.matie.dto.cart.AddCartRequest;
+import com.ost.matie.dto.cart.CartResponse;
 import com.ost.matie.dto.comment.AddCommentRequest;
+import com.ost.matie.dto.comment.CommentResponse;
 import com.ost.matie.service.cart.CartService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RequiredArgsConstructor
 @RestController
@@ -23,5 +25,15 @@ public class CartController {
         Cart cart = cartService.save(request);
         return ResponseEntity.status(HttpStatus.CREATED)
                 .body(cart);
+    }
+
+    @GetMapping("/cart/{userId}")
+    public ResponseEntity<List<CartResponse>> findAllByUserId(@PathVariable Long userId) {
+        List<CartResponse> cartResponses = cartService.findAllByUserId(userId)
+                .stream()
+                .map(CartResponse::new)
+                .toList();
+
+        return ResponseEntity.ok().body(cartResponses);
     }
 }
