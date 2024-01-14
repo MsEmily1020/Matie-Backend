a3dbc8fb09f56c968b4ebf2c4e0626cb8c0562b6
MsEmily1020 <peace10200khs@gmail.com>
2024-01-14 19:57:58 +0900

    ✨ clear repository 추가

---
+++ src/main/java/com/ost/matie/repository/ClearRepository.java
@@ -0,0 +1,12 @@
+package com.ost.matie.repository;
+
+import com.ost.matie.domain.clear.Clear;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.time.LocalDate;
+import java.util.List;
+
+public interface ClearRepository extends JpaRepository<Clear, Long> {
+    List<Clear> findAllByUserId(Long userId);
+    Clear findFirstByUserIdAndDateOrderByDateDesc(Long userId, LocalDate date);
+}
