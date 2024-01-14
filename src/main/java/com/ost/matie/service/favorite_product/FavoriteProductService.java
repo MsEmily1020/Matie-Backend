446d6c572713bfec3c3174ce2d5c273505622675
MsEmily1020 <peace10200khs@gmail.com>
2024-01-14 15:56:17 +0900

    ✨ favorite-product service - update 추가

--- src/main/java/com/ost/matie/service/favorite_product/FavoriteProductService.java
+++ src/main/java/com/ost/matie/service/favorite_product/FavoriteProductService.java
@@ -1,14 +1,12 @@
 package com.ost.matie.service.favorite_product;
 
-import com.ost.matie.domain.community.Community;
 import com.ost.matie.domain.favorite_product.FavoriteProduct;
 import com.ost.matie.dto.favorite_product.AddFavoriteProductRequest;
-import com.ost.matie.dto.favorite_product.FavoriteProductResponse;
+import com.ost.matie.dto.favorite_product.UpdateFavoriteProductRequest;
 import com.ost.matie.repository.FavoriteProductRepository;
+import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
-
-import java.util.List;
 
 @RequiredArgsConstructor
 @Service
@@ -21,5 +19,12 @@ public class FavoriteProductService {
 
     public FavoriteProduct findByUserId(Long userId) {
         return favoriteProductRepository.findByUserId(userId);
+    }
+
+    @Transactional
+    public FavoriteProduct update(Long userId, UpdateFavoriteProductRequest request) {
+        FavoriteProduct favoriteProduct = favoriteProductRepository.findByUserId(userId);
+        favoriteProduct.update(request.getProduct());
+        return favoriteProduct;
     }
 }
