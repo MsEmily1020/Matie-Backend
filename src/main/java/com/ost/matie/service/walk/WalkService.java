5c6f1418e52342c425c288ab98126f330ded7196
MsEmily1020 <peace10200khs@gmail.com>
2024-01-14 16:22:43 +0900

    ✨ walk service - update 추가

--- src/main/java/com/ost/matie/service/walk/WalkService.java
+++ src/main/java/com/ost/matie/service/walk/WalkService.java
@@ -1,8 +1,12 @@
 package com.ost.matie.service.walk;
 
+import com.ost.matie.domain.user.Users;
 import com.ost.matie.domain.walk.Walk;
+import com.ost.matie.dto.user.UpdateUserRequest;
 import com.ost.matie.dto.walk.AddWalkRequest;
+import com.ost.matie.dto.walk.UpdateWalkRequest;
 import com.ost.matie.repository.WalkRepository;
+import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -24,5 +28,12 @@ public class WalkService {
 
     public Walk findByUserIdAndDate(Long userId, LocalDate date) {
         return walkRepository.findByUserIdAndDate(userId, date);
+    }
+
+    @Transactional
+    public Walk update(Long userId, LocalDate date, UpdateWalkRequest request) {
+        Walk walk = walkRepository.findByUserIdAndDate(userId, date);
+        walk.update(request.getCount());
+        return walk;
     }
 }
