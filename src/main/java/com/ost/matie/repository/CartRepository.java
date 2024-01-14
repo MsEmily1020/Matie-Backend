43d63b33e4f3bede11b6fc242135cdccbfb4c936
MsEmily1020 <peace10200khs@gmail.com>
2024-01-15 08:26:40 +0900

    👽️ cart getMapping type List<Cart> -> Cart change

--- src/main/java/com/ost/matie/repository/CartRepository.java
+++ src/main/java/com/ost/matie/repository/CartRepository.java
@@ -6,5 +6,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
 
 public interface CartRepository extends JpaRepository<Cart, Long> {
-    List<Cart> findAllByUserId(Long userId);
+    Cart findByUserId(Long userId);
 }
