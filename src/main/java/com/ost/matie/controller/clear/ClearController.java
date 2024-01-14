15a6ef0db077a64b07f89ee493d0a7c79ccc3fc8
MsEmily1020 <peace10200khs@gmail.com>
2024-01-14 20:11:39 +0900

    ✨ clear controller - addClear 추가

--- src/main/java/com/ost/matie/controller/clear/ClearController.java
+++ src/main/java/com/ost/matie/controller/clear/ClearController.java
@@ -1,13 +1,17 @@
 package com.ost.matie.controller.clear;
 
+import com.ost.matie.domain.clear.Clear;
+import com.ost.matie.domain.comment.Comment;
+import com.ost.matie.dto.clear.AddClearRequest;
 import com.ost.matie.dto.clear.ClearResponse;
+import com.ost.matie.dto.comment.AddCommentRequest;
 import com.ost.matie.dto.comment.CommentResponse;
 import com.ost.matie.service.clear.ClearService;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -15,6 +19,13 @@ import java.util.List;
 @RestController
 public class ClearController {
     private final ClearService clearService;
+
+    @PostMapping("/clear")
+    public ResponseEntity<Clear> addClear(@Valid @RequestBody AddClearRequest request) {
+        Clear clear = clearService.save(request);
+        return ResponseEntity.status(HttpStatus.CREATED)
+                .body(clear);
+    }
 
     @GetMapping("/clear/{userId}")
     public ResponseEntity<List<ClearResponse>> findAllByUserId(@PathVariable Long userId) {
