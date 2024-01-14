ec6b8eae79d5aafed15a7fd993717b9413633466
MsEmily1020 <peace10200khs@gmail.com>
2024-01-14 16:18:48 +0900

    ✨ walk repository 추가

---
+++ src/main/java/com/ost/matie/repository/WalkRepository.java
@@ -0,0 +1,12 @@
+package com.ost.matie.repository;
+
+import com.ost.matie.domain.walk.Walk;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.time.LocalDate;
+import java.util.List;
+
+public interface WalkRepository extends JpaRepository<Walk, Long> {
+    List<Walk> findByUserId(Long userId);
+    Walk findByUserIdAndDate(Long userId, LocalDate date);
+}
