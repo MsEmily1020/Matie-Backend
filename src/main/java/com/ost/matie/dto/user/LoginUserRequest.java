faa1a8a38411b7e3c1dfabdc5f3033b893bbfd45
MsEmily1020 <peace10200khs@gmail.com>
2024-01-15 08:30:24 +0900

    ✨ user dto - login request 추가

---
+++ src/main/java/com/ost/matie/dto/user/LoginUserRequest.java
@@ -0,0 +1,19 @@
+package com.ost.matie.dto.user;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+public class LoginUserRequest {
+    @NotBlank
+    @Email
+    private String email;
+
+    @NotBlank
+    private String pw;
+}
