3854736c258184993a2fa987cb54ee037d000ddd
MsEmily1020 <peace10200khs@gmail.com>
2024-01-15 08:31:55 +0900

    ✨ user repository - findByEmailAndPw 추가

--- src/main/java/com/ost/matie/repository/UserRepository.java
+++ src/main/java/com/ost/matie/repository/UserRepository.java
@@ -4,4 +4,5 @@ import com.ost.matie.domain.user.Users;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserRepository extends JpaRepository<Users, Long> {
+    Users findByEmailAndPw(String email, String pw);
 }
