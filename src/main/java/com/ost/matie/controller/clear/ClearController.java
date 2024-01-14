d950663fecbc8102b7dd213f83882b722b8bc783
MsEmily1020 <peace10200khs@gmail.com>
2024-01-14 20:12:43 +0900

    ✨ clear controller - updateClear 추가

--- src/main/java/com/ost/matie/controller/clear/ClearController.java
+++ src/main/java/com/ost/matie/controller/clear/ClearController.java
@@ -2,10 +2,13 @@ package com.ost.matie.controller.clear;
 
 import com.ost.matie.domain.clear.Clear;
 import com.ost.matie.domain.comment.Comment;
+import com.ost.matie.domain.walk.Walk;
 import com.ost.matie.dto.clear.AddClearRequest;
 import com.ost.matie.dto.clear.ClearResponse;
+import com.ost.matie.dto.clear.UpdateClearRequest;
 import com.ost.matie.dto.comment.AddCommentRequest;
 import com.ost.matie.dto.comment.CommentResponse;
+import com.ost.matie.dto.walk.UpdateWalkRequest;
 import com.ost.matie.service.clear.ClearService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
@@ -13,6 +16,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.time.LocalDate;
 import java.util.List;
 
 @RequiredArgsConstructor
@@ -35,5 +39,13 @@ public class ClearController {
                 .toList();
 
         return ResponseEntity.ok().body(commentResponses);
+    }
+
+    @PutMapping("/clear/{userId}/{date}")
+    public ResponseEntity<Clear> updateClear(@PathVariable Long userId,
+                                           @PathVariable LocalDate date,
+                                           @RequestBody UpdateClearRequest request) {
+        Clear clear = clearService.update(userId, date, request);
+        return ResponseEntity.ok().body(clear);
     }
 }
