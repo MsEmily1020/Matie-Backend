5d5bedfbe12724ade32a116c3e5d67c07306f39f
MsEmily1020 <peace10200khs@gmail.com>
2024-01-14 16:12:35 +0900

    ✨ walk entity 추가

---
+++ src/main/java/com/ost/matie/domain/walk/Walk.java
@@ -0,0 +1,41 @@
+package com.ost.matie.domain.walk;
+
+import com.ost.matie.domain.user.Users;
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Walk {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", updatable = false)
+    private Long id;
+
+    @Column(name = "count")
+    private Long count;
+
+    @Column(name = "date")
+    private LocalDate date;
+
+    @ManyToOne
+    @JoinColumn(name = "user")
+    private Users user;
+
+    @Builder
+    public Walk(Long count, LocalDate date, Users user) {
+        this.count = count;
+        this.date = date;
+        this.user = user;
+    }
+
+    public void update(Long count) {
+        this.count = count;
+    }
+}
