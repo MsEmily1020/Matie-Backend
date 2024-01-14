5b9d7df7bd800ec17e44a69e4b027d35c61e240f
MsEmily1020 <peace10200khs@gmail.com>
2024-01-14 15:54:41 +0900

    ✨ favorite-product dto - findByUserId 추가

--- src/main/java/com/ost/matie/service/favorite_product/FavoriteProductService.java
+++ src/main/java/com/ost/matie/service/favorite_product/FavoriteProductService.java
@@ -1,10 +1,14 @@
 package com.ost.matie.service.favorite_product;
 
+import com.ost.matie.domain.community.Community;
 import com.ost.matie.domain.favorite_product.FavoriteProduct;
 import com.ost.matie.dto.favorite_product.AddFavoriteProductRequest;
+import com.ost.matie.dto.favorite_product.FavoriteProductResponse;
 import com.ost.matie.repository.FavoriteProductRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @RequiredArgsConstructor
 @Service
@@ -15,4 +19,7 @@ public class FavoriteProductService {
         return favoriteProductRepository.save(request.toEntity());
     }
 
+    public FavoriteProduct findByUserId(Long userId) {
+        return favoriteProductRepository.findByUserId(userId);
+    }
 }
