49471c92e9fe08eaf25310219d9ea0c8c10013a1
MsEmily1020 <peace10200khs@gmail.com>
2024-01-14 19:56:43 +0900

    ✨ challenge dto - update request 추가

--- src/main/java/com/ost/matie/dto/clear/AddClearRequest.java
+++ src/main/java/com/ost/matie/dto/clear/AddClearRequest.java
@@ -2,6 +2,7 @@ package com.ost.matie.dto.clear;
 
 import com.ost.matie.domain.clear.Clear;
 import com.ost.matie.domain.user.Users;
+import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -15,6 +16,8 @@ import java.util.List;
 @Setter
 public class AddClearRequest {
     private List<Long> challenge;
+
+    @NotNull
     private Users user;
 
     public Clear toEntity() {
