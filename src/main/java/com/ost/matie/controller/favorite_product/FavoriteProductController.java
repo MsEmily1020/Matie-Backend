87ae939f77fbcde037e9e331a15c8dd847cab101
MsEmily1020 <peace10200khs@gmail.com>
2024-01-14 16:04:23 +0900

    ✨ favorite-product controller - updateFavoriteProduct 추가

--- src/main/java/com/ost/matie/controller/favorite_product/FavoriteProductController.java
+++ src/main/java/com/ost/matie/controller/favorite_product/FavoriteProductController.java
@@ -4,8 +4,10 @@ import com.ost.matie.domain.community.Community;
 import com.ost.matie.domain.favorite_product.FavoriteProduct;
 import com.ost.matie.dto.community.AddCommunityRequest;
 import com.ost.matie.dto.community.CommunityResponse;
+import com.ost.matie.dto.community.UpdateCommunityRequest;
 import com.ost.matie.dto.favorite_product.AddFavoriteProductRequest;
 import com.ost.matie.dto.favorite_product.FavoriteProductResponse;
+import com.ost.matie.dto.favorite_product.UpdateFavoriteProductRequest;
 import com.ost.matie.service.favorite_product.FavoriteProductService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
@@ -31,5 +33,12 @@ public class FavoriteProductController {
 
         return ResponseEntity.ok()
                 .body(new FavoriteProductResponse(favoriteProduct));
+    }
+
+    @PutMapping("/favorite-product/{userId}")
+    public ResponseEntity<FavoriteProduct> updateFavoriteProduct(@PathVariable Long userId,
+                                                     @Valid @RequestBody UpdateFavoriteProductRequest request) {
+        FavoriteProduct favoriteProduct = favoriteProductService.update(userId, request);
+        return ResponseEntity.ok().body(favoriteProduct);
     }
 }
