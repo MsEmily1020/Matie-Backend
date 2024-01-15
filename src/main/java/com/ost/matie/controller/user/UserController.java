35d34b919575bd2f1831e86ed47b60062a7a576e
MsEmily1020 <peace10200khs@gmail.com>
2024-01-15 09:15:51 +0900

    🗃️ data sql user_id 추가

--- src/main/java/com/ost/matie/controller/user/UserController.java
+++ src/main/java/com/ost/matie/controller/user/UserController.java
@@ -28,7 +28,7 @@ public class UserController {
 
     @PostMapping("/login")
     public ResponseEntity<Users> findLoginUser(@RequestBody LoginUserRequest request) {
-        Users users = userService.findByEmailAndPw(request);
+        Users users = userService.findByUserIdAndPw(request);
         return ResponseEntity.ok().body(users);
     }
 
