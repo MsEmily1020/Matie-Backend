e161b24347fe6b2f2626223a6bb512e6d24840c2
MsEmily1020 <peace10200khs@gmail.com>
2024-01-15 13:24:39 +0900

    ✨ walk service - userFkNotFound 메소드 추가

--- src/main/java/com/ost/matie/service/walk/WalkService.java
+++ src/main/java/com/ost/matie/service/walk/WalkService.java
@@ -5,6 +5,7 @@ import com.ost.matie.dto.walk.AddWalkRequest;
 import com.ost.matie.dto.walk.UpdateWalkRequest;
 import com.ost.matie.exception.DataNotFoundException;
 import com.ost.matie.exception.DuplicateException;
+import com.ost.matie.exception.FkNotFoundException;
 import com.ost.matie.repository.WalkRepository;
 import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
@@ -19,28 +20,44 @@ public class WalkService {
     private final WalkRepository walkRepository;
 
     public Walk save(AddWalkRequest request) {
+        userFkNotFound(request.getUser().getId());
+
         if(walkRepository.existsByUserIdAndDate(request.getUser().getId(), LocalDate.now()))
             throw new DuplicateException("duplicate data by user id : " + request.getUser().getId() + ", date : " + LocalDate.now());
+
         return walkRepository.save(request.toEntity());
     }
 
     public List<Walk> findAllByUserId(Long userId) {
+        userFkNotFound(userId);
+
         List<Walk> walks = walkRepository.findAllByUserId(userId);
         if(walks.isEmpty()) throw new DataNotFoundException("No data by userId : " + userId);
+
         return walks;
     }
 
     public Walk findFirstByUserIdAndDateOrderByDateDesc(Long userId, LocalDate date) {
+        userFkNotFound(userId);
+
         Walk walk = walkRepository.findFirstByUserIdAndDateOrderByDateDesc(userId, date);
         if(walk == null) throw new DataNotFoundException("No data by userId : " + userId + ", date : " + date);
+
         return walkRepository.findFirstByUserIdAndDateOrderByDateDesc(userId, date);
     }
 
     @Transactional
     public Walk update(Long userId, LocalDate date, UpdateWalkRequest request) {
+        userFkNotFound(userId);
+
         Walk walk = walkRepository.findFirstByUserIdAndDateOrderByDateDesc(userId, date);
         if(walk == null) throw new DataNotFoundException("No data by userId : " + userId + ", date : " + date);
+
         walk.update(request.getCount());
         return walk;
+    }
+
+    public void userFkNotFound(Long userId) {
+        if(walkRepository.existsByUserId(userId)) throw new FkNotFoundException("fk null user id : " + userId);
     }
 }
