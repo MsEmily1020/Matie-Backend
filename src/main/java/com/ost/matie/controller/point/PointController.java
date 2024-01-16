f60e4ee22156f4f425f9c36e201318dd6e3d7662
MsEmily1020 <peace10200khs@gmail.com>
2024-01-16 11:47:11 +0900

    ✨ point 저장 시, user id값 있는지 확인

--- src/main/java/com/ost/matie/controller/point/PointController.java
+++ src/main/java/com/ost/matie/controller/point/PointController.java
@@ -3,7 +3,9 @@ package com.ost.matie.controller.point;
 import com.ost.matie.domain.point.Point;
 import com.ost.matie.dto.point.AddPointRequest;
 import com.ost.matie.dto.point.PointResponse;
+import com.ost.matie.exception.UserNotFoundException;
 import com.ost.matie.service.point.PointService;
+import com.ost.matie.service.user.UserService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
@@ -14,9 +16,11 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 public class PointController {
     private final PointService pointService;
+    private final UserService userService;
 
     @PostMapping("/point")
     public ResponseEntity<Point> addPoint(@Valid @RequestBody AddPointRequest request) {
+        userService.findById(request.getUser().getId());
         Point point = pointService.save(request);
         return ResponseEntity.status(HttpStatus.CREATED)
                 .body(point);
