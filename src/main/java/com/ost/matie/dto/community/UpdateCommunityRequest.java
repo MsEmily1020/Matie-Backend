9feb7e62e9f21727ecf7248fc3982538c6c6ecbe
MsEmily1020 <peace10200khs@gmail.com>
2024-01-16 12:14:14 +0900

    ✨ community exception 추가

--- src/main/java/com/ost/matie/dto/community/UpdateCommunityRequest.java
+++ src/main/java/com/ost/matie/dto/community/UpdateCommunityRequest.java
@@ -1,6 +1,7 @@
 package com.ost.matie.dto.community;
 
 import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -9,10 +10,10 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @Getter
 public class UpdateCommunityRequest {
-    @NotEmpty
+    @NotEmpty(message = "제목 칸이 비어있습니다.")
     private String title;
 
-    @NotEmpty
+    @NotEmpty(message = "설명 칸이 비어있습니다.")
     private String description;
 
     private Long agree;
