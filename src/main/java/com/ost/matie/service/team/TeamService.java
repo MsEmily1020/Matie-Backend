cdd2f9c467ab62fe556d9ccfc8c012509d8842ad
MsEmily1020 <peace10200khs@gmail.com>
2024-01-17 12:32:25 +0900

    ✨ team은 오늘 날짜가 시작일과 끝나는 일 사이에 위치하는 list만 get할 수 있게 구현

--- src/main/java/com/ost/matie/service/team/TeamService.java
+++ src/main/java/com/ost/matie/service/team/TeamService.java
@@ -8,8 +8,11 @@ import com.ost.matie.repository.TeamRepository;
 import com.ost.matie.repository.UserRepository;
 import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
+import org.springframework.cglib.core.Local;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -19,7 +22,12 @@ public class TeamService {
     private final TeamRepository teamRepository;
     private final UserRepository userRepository;
 
-    public List<Team> findAll() { return teamRepository.findAll(); }
+    public List<Team> findAll() {
+        return teamRepository
+            .findAllByStartDateLessThanEqualAndFinishDateGreaterThanEqualOrderByStartDateAsc(
+                    LocalDateTime.MIN.now(), LocalDateTime.MAX.now()
+            );
+    }
 
     public List<Team> findUserListInUserId(Long userId) {
         if(!userRepository.existsById(userId)) throw new UserNotFoundException("사용자를 찾을 수 없습니다. (id : " + userId + ")");
