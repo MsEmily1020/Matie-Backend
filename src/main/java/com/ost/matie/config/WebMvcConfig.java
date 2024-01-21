45f3a8173534242f40f6b3be23a47b959aadf42c
MsEmily1020 <peace10200khs@gmail.com>
2024-01-21 20:52:23 +0900

    🐛 cors 에러 해결

--- src/main/java/com/ost/matie/config/WebMvcConfig.java
+++ src/main/java/com/ost/matie/config/WebMvcConfig.java
@@ -1,6 +1,7 @@
 package com.ost.matie.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -10,5 +11,11 @@ public class WebMvcConfig implements WebMvcConfigurer {
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
         registry.addResourceHandler("/**")
                 .addResourceLocations("file:src/main/resources/static/");
+    }
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("http://localhost:8080", "http://127.0.0.1:5500");
     }
 }
