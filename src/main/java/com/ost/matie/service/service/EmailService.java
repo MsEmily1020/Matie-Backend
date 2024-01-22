d94b574a25641f7a4040dd5aae7e59daf7dfac84
MsEmily1020 <peace10200khs@gmail.com>
2024-01-22 14:10:08 +0900

    ✨ redis 연결 및 만료 코드 추가

--- src/main/java/com/ost/matie/service/service/EmailService.java
+++ src/main/java/com/ost/matie/service/service/EmailService.java
@@ -1,5 +1,6 @@
 package com.ost.matie.service.service;
 
+import com.ost.matie.config.RedisUtil;
 import com.ost.matie.dto.email.AddEmailRequest;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -7,22 +8,30 @@ import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Service;
 
+import java.util.Random;
+
 @RequiredArgsConstructor
 @Service
 public class EmailService {
     @Autowired
     private JavaMailSender javaMailSender;
+    private final RedisUtil redisUtil;
+    private String authKey;
 
     public void sendMessage(AddEmailRequest request) {
+        authKey = createCode();
+
         SimpleMailMessage message = new SimpleMailMessage();
         message.setFrom("peace10200khs@gmail.com");
         message.setTo(request.getEmailAddress());
         message.setSubject("[Matie] 사용자 인증 이메일 코드");
-        message.setText(createCode() + "");
+        message.setText(authKey);
         javaMailSender.send(message);
+
+        redisUtil.setDataExpire(authKey, request.getEmailAddress(), 60 * 5L);
     }
 
-    public Integer createCode() {
-        return (int)(Math.random() * 999999 + 1);
+    public String createCode() {
+        return String.valueOf(new Random().nextInt(888888) + 111111);
     }
 }
