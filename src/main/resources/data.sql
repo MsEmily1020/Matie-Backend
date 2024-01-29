24366518faeb3d4b97e7e11abb49eaeba0c9492a
MsEmily1020 <peace10200khs@gmail.com>
2024-01-29 19:56:14 +0900

    🔧 querydsl 종속성 추가

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -2,6 +2,18 @@ INSERT INTO image (url, type) VALUES ('http://localhost:8080/character/1.svg', 2
 INSERT INTO image (url, type) VALUES ('http://localhost:8080/character/2.svg', 2);
 INSERT INTO image (url, type) VALUES ('http://localhost:8080/character/3.svg', 2);
 INSERT INTO image (url, type) VALUES ('http://localhost:8080/character/4.svg', 2);
+INSERT INTO image (url, type) VALUES ('http://localhost:8080/products/1.svg', 3);
+INSERT INTO image (url, type) VALUES ('http://localhost:8080/products/2.svg', 3);
+INSERT INTO image (url, type) VALUES ('http://localhost:8080/products/3.svg', 3);
+INSERT INTO image (url, type) VALUES ('http://localhost:8080/products/4.svg', 3);
+INSERT INTO image (url, type) VALUES ('http://localhost:8080/products/5.svg', 3);
+INSERT INTO image (url, type) VALUES ('http://localhost:8080/products/6.svg', 3);
+INSERT INTO image (url, type) VALUES ('http://localhost:8080/products/7.svg', 3);
+INSERT INTO image (url, type) VALUES ('http://localhost:8080/products/8.svg', 3);
+INSERT INTO image (url, type) VALUES ('http://localhost:8080/products/9.svg', 3);
+INSERT INTO image (url, type) VALUES ('http://localhost:8080/products/10.svg', 3);
+INSERT INTO image (url, type) VALUES ('http://localhost:8080/products/11.svg', 3);
+INSERT INTO image (url, type) VALUES ('http://localhost:8080/products/12.svg', 3);
 
 INSERT INTO users (name, user_id, email, pw, created_date) VALUES ('김혜승', 'hyeseung', 's2224@e-mirim.hs.kr', '$2a$10$tEM8I25AuRIaY61FrEEBDe6ljr5F17UEzl50pjZoPe40m0gZrifA6', '2023-10-20 10:11:20');
 INSERT INTO point (variation, balance, user_id) VALUES (0, 0, 1);
