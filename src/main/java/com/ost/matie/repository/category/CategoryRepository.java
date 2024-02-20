3eba5b9b47b1ec7116014dcb33068f30129e2cf1
MsEmily1020 <peace10200khs@gmail.com>
2024-02-21 00:24:12 +0900

    Remove:: category service 삭제

--- src/main/java/com/ost/matie/repository/category/CategoryRepository.java
+++
@@ -1,7 +0,0 @@
-package com.ost.matie.repository.category;
-
-import com.ost.matie.domain.category.Category;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CategoryRepository extends JpaRepository<Category, Integer> {
-}
