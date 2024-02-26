77d4fc207fe63d65caa8e6e46510a42790fb3b01
MsEmily1020 <peace10200khs@gmail.com>
2024-02-26 17:46:48 +0900

    Refactor:: Image Service 이름 변경

--- src/main/java/com/ost/matie/controller/image/ImageController.java
+++ src/main/java/com/ost/matie/controller/image/ImageController.java
@@ -1,9 +1,8 @@
 package com.ost.matie.controller.image;
 
-import com.ost.matie.dto.image.ImageResponse;
+import com.ost.matie.domain.image.Image;
 import com.ost.matie.service.image.ImageService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -16,12 +15,7 @@ public class ImageController {
     private final ImageService imageService;
 
     @GetMapping("/image/{type}")
-    public ResponseEntity<List<ImageResponse>> findAllByTypeImage(@PathVariable Integer type) {
-        List<ImageResponse> imageResponses = imageService.findAllByType(type)
-                .stream()
-                .map(ImageResponse::new)
-                .toList();
-
-        return ResponseEntity.ok().body(imageResponses);
+    public List<Image> findAllByTypeImage(@PathVariable Integer type) {
+        return imageService.execute(type);
     }
 }
--- src/main/java/com/ost/matie/service/image/ImageService.java
+++ src/main/java/com/ost/matie/service/image/ImageService.java
@@ -3,6 +3,7 @@ package com.ost.matie.service.image;
 import com.ost.matie.domain.image.Image;
 import com.ost.matie.exception.TypeNotFoundException;
 import com.ost.matie.repository.image.ImageRepository;
+import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -13,7 +14,8 @@ import java.util.List;
 public class ImageService {
     private final ImageRepository imageRepository;
 
-    public List<Image> findAllByType(Integer type) {
+    @Transactional
+    public List<Image> execute(Integer type) {
         if(!(type == 1 || type == 2 || type == 3 || type == 4)) throw TypeNotFoundException.EXCEPTION;
         return imageRepository.findAllByType(type);
     }
