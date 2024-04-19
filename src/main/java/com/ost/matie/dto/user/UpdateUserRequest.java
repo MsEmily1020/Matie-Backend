887a2d6450a8815e2dc1f472b48da733542cb28d
MsEmily1020 <peace10200khs@gmail.com>
2024-04-19 09:50:00 +0900

    Refactor :: user dto @Pattern -> @Password으로 변경

--- src/main/java/com/ost/matie/dto/user/UpdateUserRequest.java
+++ src/main/java/com/ost/matie/dto/user/UpdateUserRequest.java
@@ -1,7 +1,7 @@
 package com.ost.matie.dto.user;
 
+import com.ost.matie.annotation.Password;
 import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.Pattern;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -11,6 +11,6 @@ import lombok.NoArgsConstructor;
 @Getter
 public class UpdateUserRequest {
     @NotBlank(message = "비밀번호 칸이 비어있습니다.")
-    @Pattern(regexp = "(?=.*[0-9])(?=.*[a-zA-Z])(?=.*\\W)(?=\\S+$).{8,16}", message = "8~20자 이내로 영문과 숫자, 특수기호가 들어가야 합니다.")
+    @Password
     private String pw;
 }
