9d556c85e3fc30b4d82171a40333e3f1a3190f12
MsEmily1020 <peace10200khs@gmail.com>
2024-06-17 22:19:29 +0900

    add: challenge confirm get service 추가

---
+++ src/main/java/com/ost/matie/domain/challenge/service/challenge_confirm/GetChallengeConfirmService.java
@@ -0,0 +1,26 @@
+package com.ost.matie.domain.challenge.service.challenge_confirm;
+
+import com.ost.matie.domain.challenge.ChallengeConfirm;
+import com.ost.matie.domain.challenge.dto.ChallengeConfirmResponse;
+import com.ost.matie.domain.challenge.exception.ChallengeNotFoundException;
+import com.ost.matie.domain.challenge.repository.ChallengeConfirmRepository;
+import com.ost.matie.domain.challenge.repository.ChallengeRepository;
+import com.ost.matie.domain.user.exception.UserNotFoundException;
+import com.ost.matie.domain.user.repository.UserRepository;
+import com.ost.matie.global.annotation.ReadOnlyService;
+import lombok.RequiredArgsConstructor;
+
+@ReadOnlyService
+@RequiredArgsConstructor
+public class GetChallengeConfirmService {
+    private final ChallengeConfirmRepository challengeConfirmRepository;
+    private final ChallengeRepository challengeRepository;
+    private final UserRepository userRepository;
+
+    public ChallengeConfirmResponse execute(Long userId, Long challengeId) {
+        if(!userRepository.existsById(userId)) throw UserNotFoundException.EXCEPTION;
+        if(!challengeRepository.existsById(challengeId)) throw ChallengeNotFoundException.EXCEPTION;
+        ChallengeConfirm challengeConfirm = challengeConfirmRepository.findByChallengeIdAndUserId(challengeId, userId);
+        return new ChallengeConfirmResponse(challengeConfirm);
+    }
+}
