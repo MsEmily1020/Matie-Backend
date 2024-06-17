10a0e2aff4c4c3de3ef894835bb6255c306c397f
MsEmily1020 <peace10200khs@gmail.com>
2024-06-17 21:40:43 +0900

    add: challenge confirm repository 추가

---
+++ src/main/java/com/ost/matie/domain/challenge/repository/ChallengeConfirmRepository.java
@@ -0,0 +1,7 @@
+package com.ost.matie.domain.challenge.repository;
+
+import com.ost.matie.domain.challenge.ChallengeConfirm;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ChallengeConfirmRepository extends JpaRepository<ChallengeConfirm, Long> {
+}
