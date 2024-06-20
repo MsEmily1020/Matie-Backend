ad56a01804ea6d84aa95bd8a52c82d940b953607
MsEmily1020 <peace10200khs@gmail.com>
2024-06-20 11:07:04 +0900

    fix: WebMvcConfig addResource 설정 변경

--- src/main/java/com/ost/matie/global/config/WebMvcConfig.java
+++ src/main/java/com/ost/matie/global/config/WebMvcConfig.java
@@ -10,7 +10,7 @@ public class WebMvcConfig implements WebMvcConfigurer {
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
         registry.addResourceHandler("/**")
-                .addResourceLocations("classpath:/");
+                .addResourceLocations("file:src/main/resources/static/");
     }
 
     @Override
